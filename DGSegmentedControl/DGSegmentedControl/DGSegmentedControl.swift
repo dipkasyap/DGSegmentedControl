b76294f9785496f29be7b1cdac60565bd4d3062b
dip <dipdirge@gmail.com>
2016-09-20 16:37:23 +0545

    Updated for Swift 3.0

--- DGSegmentedControl/DGSegmentedControl/DGSegmentedControl.swift
+++ DGSegmentedControl/DGSegmentedControl/DGSegmentedControl.swift
@@ -23,13 +23,13 @@ import UIKit
         }
     }
     
-    @IBInspectable var selectedLabelColor : UIColor = UIColor.whiteColor() {
+    @IBInspectable var selectedLabelColor : UIColor = UIColor.white {
         didSet {
             setSelectedColors()
         }
     }
     
-    @IBInspectable var unselectedLabelColor : UIColor = UIColor.blackColor() {
+    @IBInspectable var unselectedLabelColor : UIColor = UIColor.black {
         didSet {
             setSelectedColors()
         }
@@ -47,7 +47,7 @@ import UIKit
         }
     }
     
-    @IBInspectable var font : UIFont! = UIFont.systemFontOfSize(12) {
+    @IBInspectable var font : UIFont! = UIFont.systemFont(ofSize: 12) {
         didSet {
             setFont()
         }
@@ -77,10 +77,10 @@ import UIKit
     }
     
     func setupView(){
-        backgroundColor = UIColor.clearColor()
+        backgroundColor = UIColor.clear
         setupLabels()
-        addIndividualItemConstraints(labels, mainView: self, padding: 0)
-        insertSubview(thumbView, atIndex: 0)
+        addIndividualItemConstraints(items: labels, mainView: self, padding: 0)
+        insertSubview(thumbView, at: 0)
     }
     
     func setBorder(){
@@ -93,14 +93,14 @@ import UIKit
             label.removeFromSuperview()
         }
         
-        labels.removeAll(keepCapacity: true)
+        labels.removeAll(keepingCapacity: true)
         
         for index in 1...items.count {
             
-            let label = UILabel(frame: CGRectMake(0, 5, 70, 40))
+            let label = UILabel(frame:CGRect(x: 0, y: 5, width: 70, height: 40))
             label.text = items[index - 1]
-            label.backgroundColor = UIColor.clearColor()
-            label.textAlignment = .Center
+            label.backgroundColor = UIColor.clear
+            label.textAlignment = .center
             label.font = UIFont(name: "Avenir-Black", size: 15)
             label.textColor = index == 1 ? selectedLabelColor : unselectedLabelColor
             label.translatesAutoresizingMaskIntoConstraints = false
@@ -108,12 +108,12 @@ import UIKit
             labels.append(label)
         }
         
-        addIndividualItemConstraints(labels, mainView: self, padding: 0)
+        addIndividualItemConstraints(items: labels, mainView: self, padding: 0)
     }
     
     func setViewForLabel(label: UILabel) {
         var oldFrame = label.bounds
-        let newWidth = CGRectGetWidth(oldFrame) - 50
+        let newWidth = oldFrame.width - 50
         oldFrame.size.width = newWidth
         label.frame = oldFrame
         
@@ -125,7 +125,7 @@ import UIKit
         super.layoutSubviews()
         
         var selectFrame = self.bounds
-        let newWidth = CGRectGetWidth(selectFrame) / CGFloat(items.count)
+        let newWidth = selectFrame.width / CGFloat(items.count)
         selectFrame.size.width = newWidth
         thumbView.frame = selectFrame
         thumbView.backgroundColor = thumbColor
@@ -133,12 +133,12 @@ import UIKit
         
     }
     
-    override func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         
-        let location = touch.locationInView(self)
+        let location = touch.location(in: self)
         
         var calculatedIndex : Int?
-        for (index, item) in labels.enumerate() {
+        for (index, item) in labels.enumerated() {
             if item.frame.contains(location) {
                 calculatedIndex = index
             }
@@ -147,21 +147,21 @@ import UIKit
         
         if calculatedIndex != nil {
             selectedIndex = calculatedIndex!
-            sendActionsForControlEvents(.ValueChanged)
+            sendActions(for: .valueChanged)
         }
         
         return false
     }
     
     func displayNewSelectedIndex(){
-        for (_, item) in labels.enumerate() {
+        for (_, item) in labels.enumerated() {
             item.textColor = unselectedLabelColor
         }
         
         let label = labels[selectedIndex]
         label.textColor = selectedLabelColor
         
-        UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.8, options: UIViewAnimationOptions.AllowAnimatedContent, animations: {
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.8, options: UIViewAnimationOptions.allowAnimatedContent, animations: {
             
             var newFrame = label.frame
             newFrame.origin.y = newFrame.height-self.thumUnderLineSize
@@ -174,22 +174,22 @@ import UIKit
     
     func addIndividualItemConstraints(items: [UIView], mainView: UIView, padding: CGFloat) {
         
-        for (index, button) in items.enumerate() {
+        for (index, button) in items.enumerated() {
             
-            let topConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: mainView, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0)
+            let topConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: mainView, attribute: NSLayoutAttribute.top, multiplier: 1.0, constant: 0)
             
-            let bottomConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: mainView, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0)
+            let bottomConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: mainView, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 0)
             
             var rightConstraint : NSLayoutConstraint!
             
             if index == items.count - 1 {
                 
-                rightConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: mainView, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: -padding)
+                rightConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: mainView, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: -padding)
                 
             }else{
                 
                 let nextButton = items[index+1]
-                rightConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: nextButton, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: -padding)
+                rightConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.right, relatedBy: NSLayoutRelation.equal, toItem: nextButton, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: -padding)
             }
             
             
@@ -197,16 +197,16 @@ import UIKit
             
             if index == 0 {
                 
-                leftConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: mainView, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: padding)
+                leftConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: mainView, attribute: NSLayoutAttribute.left, multiplier: 1.0, constant: padding)
                 
             }else{
                 
                 let prevButton = items[index-1]
-                leftConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: prevButton, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: padding)
+                leftConstraint = NSLayoutConstraint(item: button, attribute: NSLayoutAttribute.left, relatedBy: NSLayoutRelation.equal, toItem: prevButton, attribute: NSLayoutAttribute.right, multiplier: 1.0, constant: padding)
                 
                 let firstItem = items[0]
                 
-                let widthConstraint = NSLayoutConstraint(item: button, attribute: .Width, relatedBy: NSLayoutRelation.Equal, toItem: firstItem, attribute: .Width, multiplier: 1.0  , constant: 0)
+                let widthConstraint = NSLayoutConstraint(item: button, attribute: .width, relatedBy: NSLayoutRelation.equal, toItem: firstItem, attribute: .width, multiplier: 1.0  , constant: 0)
                 
                 mainView.addConstraint(widthConstraint)
             }
@@ -239,8 +239,8 @@ extension UIView {
     func setUnderline(ofColor color:UIColor, andSize sized:CGFloat){
         //Making UnderLIne BOrder on UitextFld
         let underLine = CALayer()
-        underLine.frame = CGRectMake(0.0, self.frame.size.height - 1, self.frame.size.width, sized);
-        underLine.backgroundColor = color.CGColor
+        underLine.frame = CGRect(x: 0, y: self.frame.size.height-1, width: self.frame.size.width, height: sized)
+        underLine.backgroundColor = color.cgColor
         self.layer.addSublayer(underLine)
     }
     
--- DGSegmentedControl/ViewController.swift
+++ DGSegmentedControl/ViewController.swift
@@ -24,7 +24,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //MARK:- Action
+    @IBAction func segmentValueChanged(_ sender: AnyObject) {
+        
+        if sender.selectedIndex == 0{
+            self.displayGround.backgroundColor = UIColor.gray
+            self.info.text = "First Segment selected"
+        } else {
+            self.displayGround.backgroundColor = UIColor(red: 0.761, green: 0.722, blue: 0.580, alpha: 1.00)
+            self.info.text = "Second Segment selected"
+        }
+    }
     
+    //MARK:- Segment control
     func decorateSegmentedControl(){
         
         segmentedControl.items = ["First Segment","Second Segment"]
@@ -33,21 +45,10 @@ class ViewController: UIViewController {
         segmentedControl.selectedIndex = 0
         segmentedControl.borderSize = 2
         segmentedControl.thumbColor = UIColor(red: 0.988, green: 0.820, blue: 0.447, alpha: 1.00)
-        segmentedControl.selectedLabelColor = UIColor.blackColor()
+        segmentedControl.selectedLabelColor = UIColor.black
         segmentedControl.thumUnderLineSize = 8
-        segmentedControl.font = UIFont.systemFontOfSize(18)
-        segmentedControl.addTarget(self, action: "segmentValueChanged:", forControlEvents: .ValueChanged)
+        segmentedControl.font = UIFont.systemFont(ofSize: 18)
         self.segmentValueChanged(self.segmentedControl)
     }
     
-    func segmentValueChanged(sender:DGSegmentedControl){
-        
-        if sender.selectedIndex == 0{
-            self.displayGround.backgroundColor = UIColor.grayColor()
-            self.info.text = "First Segment selected"
-        } else {
-            self.displayGround.backgroundColor = UIColor(red: 0.761, green: 0.722, blue: 0.580, alpha: 1.00)
-            self.info.text = "Second Segment selected"
-        }
-    }
 }
